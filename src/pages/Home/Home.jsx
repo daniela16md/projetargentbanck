77f9625ea467746014bb3eb9fe12144e45c3459b
Daniela <danamattl@yahoo.com>
2024-11-24 19:25:46 +0100

    deplacer le composant features directement dans la page home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import Hero from '../../components/Hero/Hero';
-import Features from '../../components/Features/Features';
+import data from '../../Data/data.json';
 import './Home.css'
 
 function Home() {
   return (
     <div>
       <Hero />
-      <Features />
+        <div className='features'>
+          {data.article.map((item) => (
+          <div key={item.id} className="features-item">
+            <div className="features-img">
+              <img src={item.icon} alt={item.altIcon} />
+            </div>
+            <div className="features-text">
+              <div className="features-title">{item.title}</div>
+              <div className="features-subtitle">{item.paragraph}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    
     </div>
   )
 }
